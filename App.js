29fdc791ed083fdae457a39f7e828ea4602304d6
JaanuGopan <sjanugopan@gmail.com>
2023-12-07 15:02:26 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import './index.js'
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <LoginSignup/> */}
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
